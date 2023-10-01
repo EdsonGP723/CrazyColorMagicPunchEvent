12cee763f8b490aff17759eb75b6f06713227ce0
EdsonGP723 <95706015+EdsonGP723@users.noreply.github.com>
2023-10-01 17:35:42 -0600

    Organizando carpetas y avanzando en sistema de golpe

---
+++ Assets/Project/Scripts/PunchSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PunchSystem : MonoBehaviour
+{
+    public Rigidbody targetR, targetL;
+    private PlayerInput _inputs;
+    [SerializeField] private float _force;
+    private void Update() {
+        
+        
+    }
+
+}
