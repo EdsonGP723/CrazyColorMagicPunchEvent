8c7330d528d0650e1e853b0fe7aa3490d0ad80b9
EdsonGP723 <95706015+EdsonGP723@users.noreply.github.com>
2023-10-02 11:34:31 -0600

    Avanzando En golpe

--- Assets/Project/Scripts/PunchSystem.cs
+++ Assets/Project/Scripts/PunchSystem.cs
@@ -5,12 +5,36 @@ using UnityEngine.InputSystem;
 
 public class PunchSystem : MonoBehaviour
 {
-    public Rigidbody targetR, targetL;
-    private PlayerInput _inputs;
-    [SerializeField] private float _force;
-    private void Update() {
+    public PlayerInput inputs;
+    [Header ("Target L")]
+    public Transform targetL;
+    public Vector3 targetLVec;
+    public Transform targetLPos;
+    [Header ("Target R")]
+    public Transform targetR;
+    public Transform targetRPos;
+    
+    private void Start() {
         
+    }
+    
+    private void Update() {
+        if (inputs.actions["PunchL"].WasPressedThisFrame()){
+            Debug.Log("Golpe izquierdo");
+            StartCoroutine(PunchL());
+        }
         
     }
 
+    public IEnumerator PunchL(){
+        Vector3 Adelante = Direction();
+        yield return new WaitForSeconds(.3f);
+        targetL.position = targetLPos.position;
+    }
+
+    public Vector3 Direction(){
+        Vector3 Foward = targetL.forward;
+        Foward.Normalize();
+        return Foward;
+    }
 }
